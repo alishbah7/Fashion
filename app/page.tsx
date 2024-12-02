3ba2c92b2cf10db5aca9245e62954641252bdf07
alisbah7 <alishbah04ak@gmail.com>
2024-12-03 01:56:09 +0500

    Fashion - Figma Cloning

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 // import { Button } from "@/components/ui/button"
 import Header from "@/components/header";
 import Hero from "@/components/hero";
